07fa587417c101247bfd7f1bfee105ee352f1024
Alexey Volkov <volkov.aa@phystech.edu>
2020-10-25 01:49:25 +0300

    Implemented product cards getter, added fields to JSON files

--- page_build.py
+++ page_build.py
@@ -3,6 +3,7 @@ import typing
 import user_types as ut
 import feature_deduction as fd
 import glob
+import random
 from user_types import IntermediateRepresentation
 
 
@@ -46,23 +47,31 @@ def get_investment_description_path(asset_type: int, education: int):
     else:
         path += "poor_education_"
 
-    if asset_type == fd.BONDS:
-        return path + "obligations.txt"
-    elif asset_type == fd.SHARES:
-        return path + "shares.txt"
-    else:
-        return path + "structural_products.txt"
+    return path + product_category_to_name(asset_type) + ".txt"
+
 
 def product_category_to_name(product_class: int) -> str:
     if product_class == fd.SHARES:
         return "shares"
     elif product_class == fd.STRUCTURED:
-        return "structural_packages"
+        return "structural_products"
     elif product_class == fd.BONDS:
         return "obligations"
 
-def get_product_list(product_class: int) -> [str]:
-    if()
+
+def get_product_from_category(user: IntermediateRepresentation, category: int) -> str:
+    products = glob.glob("assets/cards/info/" +
+                         product_category_to_name(category) + "/*.json")
+
+    # Rather sophisticated data science algorithm
+    return random.choice(products)
+
+
+def get_product_cards(user: IntermediateRepresentation) -> Tuple[str, str, str]:
+    leftover_category = (set((fd.BONDS, fd.STRUCTURED, fd.SHARES)) - set(user.assets))[0] 
+    cat_args = (user.assets[0], user.assets[1], leftover_category)
+
+    return tuple([get_product_from_category(x, user) for x in cat_args])
 
 
 def get_investment_descriptions(user: IntermediateRepresentation) -> Tuple[str, str]:
